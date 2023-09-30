236b36ef58db204fd6e5a0efb496b65817788af7
orwithout <he.haifeng@hotmail.com>
2023-09-30 12:51:17 +0800

    接下来要做

--- svelte/release.sh
+++ svelte/release.sh
@@ -1,4 +1,4 @@
-cd svelte/ || { echo "svelte/ not found"; exit 1; }
+# cd svelte/ || { echo "svelte/ not found"; exit 1; }
 npm run build || { echo "npm run build failed"; exit 1; }
 sudo rm -rf /var/www/svelte
 cd .. || { echo "cd .. failed"; exit 1; }
